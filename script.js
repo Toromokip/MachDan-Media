2167eebb9aca3947747192393c5bf882f0fa42f9
Toromokip <124120690+Toromokip@users.noreply.github.com>
2026-01-08 21:22:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function sendMail(){
-    let parms = {
-        name : document.getElementById("name").value,
-        number : document.getElementById("number").value,
-        event: document.getElementById("event").value,
-        details : document.getElementById("details").value,
-    }
-    
+function sendMail(event) {
     event.preventDefault();
+    
+    const params = {
+        name: document.getElementById("name").value,
+        number: document.getElementById("number").value,
+        event: document.getElementById("event").value,
+        details: document.getElementById("details").value
+    };
 
-    emailjs.send("service_l57rlog","template_zrkupci",parms).then(alert("Thank for contacting us. We'll get back to you shortly!"));
+    emailjs.send("service_l57rlog", "template_zrkupci", params)
+        .then(() => {
+            alert("Thank you for contacting us. We'll get back to you shortly!");
+            document.querySelector(".contact-form").reset();
+        })
+        .catch((error) => {
+            console.error("Email send error:", error);
+            alert("Sorry, there was an error sending your message. Please try again.");
+        });
 }
